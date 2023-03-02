c8b351239231af4c142819e4896bea314186aa77
Veco Chen <vecochen@MacBook-Air-2.local>
2023-03-02 18:39:05 +0000

    create table for staff db

---
+++ server/staffDB.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE sports_management; 
+
+CREATE TABLE customers(
+    customer_ID SERIAL PRIMARY KEY,
+    description VARCHAR(255)
+);
+
+CREATE TABLE activities(
+    activity_ID SERIAL PRIMARY KEY,
+    description VARCHAR(255)
+);
+
+CREATE TABLE staff(
+    staff_ID int PRIMARY KEY,
+    staff_Name VARCHAR(255),
+    staff_Number int,
+    staff_Email VARCHAR(255),
+    staff_Title VARCHAR(255)
+);
