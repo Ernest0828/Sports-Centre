cc2599e3cdf8a2d09af5938b2c7b341cf7a85ada
Veco Chen <vecochen@MacBook-Air-2.local>
2023-03-09 15:39:58 +0000

    updated staffDB

--- server/database/models/staffDB.sql
+++ server/database/models/staffDB.sql
@@ -5,13 +5,8 @@ CREATE TABLE customers(
     description VARCHAR(255)
 );
 
-CREATE TABLE activities(
-    activity_ID SERIAL PRIMARY KEY,
-    description VARCHAR(255)
-);
-
 CREATE TABLE staff(
-    staff_ID int PRIMARY KEY,
+    staff_ID SERIAL PRIMARY KEY,
     staff_Name VARCHAR(255),
     staff_Number int,
     staff_Email VARCHAR(255),
--- server/routes/staffRoutes.js
+++ server/routes/staffRoutes.js
@@ -76,96 +76,18 @@ router.delete("/customer/:id", async(req, res) => {
     }
 })
 
-//create new activity//
-
-router.post("/activities", async (req, res)=>{
-
-    try {
-
-        //console.log(req.body);
-        const { description } = req.body;
-        const newActivity = await pool.query("INSERT INTO activities (description) VALUES($1) RETURNING *", //adding data so RETURNING gets back the data, same for update
-        [description]
-        );
-
-        res.json(newActivity.rows[0]); //where the data is located at
-        
-    } catch (err) {
-        console.error(err.message);
-    }
-
-})
-
-//get all activity//
-
-router.get("/activities", async(req, res) => {
-    try {
-        const allActivities = await pool.query("SELECT * from activities"); //no returning * because SELECT does that
-        res.json(allActivities.rows);
-    } catch (err) {
-        console.error(err.message);
-    }
-})
-
-//get one activity//
-
-router.get("/activities/:id", async(req, res) => {
-    try {
-        const {id} = req.params;
-        const activity = await pool.query("SELECT * from activities WHERE activity_id = $1", [id]) //where clause to specify 
-
-        res.json(activity.rows[0]); 
-    } catch (err) {
-        console.error(err.message);
-    }
-})
-
-//update an activity//
-
-router.put("/activities/:id", async(req, res) => {
-    try {
-        const {id} = req.params;
-        const {description} = req.body;
-        const updateActivity = await pool.query("UPDATE activities SET description = $1 WHERE activity_id = $2", //$xxx variable holder w params
-        [description, id]
-        ); //dolar signs are variable holders
-
-        res.json("Activity details updated");
-    } catch (err) {
-        console.error(err.message);
-    }
-})
-
-
-//delete an activity//
-
-router.delete("/activities/:id", async(req, res) => {
-    try {
-        const {id} = req.params;
-        const deleteActivity = await pool.query("DELETE FROM activities WHERE activity_id = $1", 
-        [id]);
-
-        res.json("Activity deleted from database");
-    } catch (err) {
-        console.error(err.message);
-    }
-})
-
 
 //create new staff//
 
 router.post("/staff", async (req, res)=>{
-
+    //console.log(req.body);
+    const { staff_name } = req.body;
+    const { staff_number } = req.body;
+    const { staff_email } = req.body;
+    const { staff_title } = req.body;
     try {
-
-        //console.log(req.body);
-        const { staff_id } = req.body;
-        const { staff_name } = req.body;
-        const { staff_number } = req.body;
-        const { staff_email } = req.body;
-        const { staff_title } = req.body;
-        const newStaff = await pool.query("INSERT INTO staff (staff_id, staff_name, staff_number, staff_email, staff_title) VALUES($1, $2, $3, $4, $5) RETURNING *", //adding data so RETURNING gets back the data, same for update
-        [staff_id, staff_name, staff_number, staff_email, staff_title]
+        const newStaff = await pool.query("INSERT INTO staff (staff_name, staff_number, staff_email, staff_title) VALUES($1, $2, $3, $4) RETURNING *", //adding data so RETURNING gets back the data, same for update
+        [staff_name, staff_number, staff_email, staff_title]
         );
 
         res.json(newStaff.rows[0]); //where the data is located at
@@ -201,37 +123,34 @@ router.get("/staff/:id", async(req, res) => {
 })
 
 //update a staff//
-
 //dolar signs are variable holders
-/*app.put("/staff/:id", async(req, res) => {
+
+router.put("/staff/:id", async(req, res) => {
     try {
         const {id} = req.params;
         const {staff_name} = req.body;
         const {staff_number} = req.body;
         const {staff_email} = req.body;
         const {staff_title} = req.body;
-        const updateStaff = await pool.query("UPDATE staff SET staff_name = $1 WHERE staff_id = $2", //$xxx variable holder w params
-        [staff_name, id]) */
-        
-        /*const updateStaff = await pool.query("UPDATE staff SET (staff_name = $1, staff_number = $2, staff_email = $3, staff_title = $4) WHERE staff_id = $5", //$xxx variable holder w params
-        [staff_name, staff_number, staff_email, staff_title, staff_id])*/
+        const updateStaff = await pool.query("UPDATE staff SET staff_name = $2, staff_number = $3, staff_email = $4, staff_title = $5 WHERE staff_id = $1", //$xxx variable holder w params
+        [id, staff_name, staff_number, staff_email, staff_title]);
 
-        /*res.json("Staff details updated");
+        res.json("Staff details updated");
     } catch (err) {
         console.error(err.message);
     }
-})*/
+})
 
 
 //delete a staff//
 
-router.delete("/activities/:id", async(req, res) => {
+router.delete("/staff/:id", async(req, res) => {
     try {
         const {id} = req.params;
-        const deleteActivity = await pool.query("DELETE FROM activities WHERE activity_id = $1", 
+        const deleteStaff = await pool.query("DELETE FROM staff WHERE staff_id = $1", 
         [id]);
 
-        res.json("Activity deleted from database");
+        res.json("Staff deleted from database");
     } catch (err) {
         console.error(err.message);
     }
