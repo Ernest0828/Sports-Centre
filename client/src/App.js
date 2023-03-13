7ce3b2b1a6240f7c8509f0db02b9522385bdbb8b
Edmund Chia <sc21ewkc@leeds.ac.uk>
2023-03-13 15:49:43 +0000

    Removed unrequired files

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React,{Fragment, useState} from 'react';
 import './App.css';
+import {BrowserRouter as Router, 
+  Routes, 
+  Route, 
+  Navigate
+} from 'react-router-dom';
+
+//components
+import Dashboard from "./components/pages/dashboard/Dashboard";
+import Login from "./components/pages/login/Login";
+import Register from "./components/pages/register/Register";
+import Profile from "./components/pages/profile/Profile";
+import Facilities from './components/pages/facilities/Facilities';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Facilities/>
+  // const [isAuthenticated, setIsAuthenticated] = useState(false);
+  // const setAuth = boolean => {
+  //   console.log("Setting auth to: ",boolean );
+  //   setIsAuthenticated(boolean);
+  //   console.log("Auth is: ",isAuthenticated );
+  // };
+
+  // return (
+  //   <Fragment>
+  //     <Router>
+  //       <div className="container">
+  //         <Routes>
+  //           <Route path="/register" 
+  //           element = {!isAuthenticated? (<Register setAuth={setAuth}/>) : (<Navigate to ="/login"/>)}/>
+  //           <Route path="/login" 
+  //           element = {!isAuthenticated? (<Login setAuth={setAuth}/>) : (<Navigate to ="/dashboard"/>)}/>
+  //           <Route path="/dashboard" 
+  //           element = {!isAuthenticated? (<Dashboard setAuth={setAuth}/>) : (<Navigate to ="/login"/>)}/>
+
+  //         </Routes>
+  //       </div>
+        
+  //     </Router>
+  //   </Fragment>
+  // );
 }
 
 export default App;
