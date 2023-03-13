b7243ae525c119c1e089bcf96394e172c782e9ff
Najmi Shahir <sc21anbm@leeds.ac.uk>
2023-03-13 22:26:40 +0000

    add staff jwt generator

--- server/utils/jwtGenerator.js
+++ server/utils/jwtGenerator.js
@@ -6,9 +6,13 @@ const EXPIRATION = "2h";
 
 // function that generates a JWT for a given customer ID
 // returns the generated JWT
-function jwtGenerator(custId) {
-    const payload = { customer: { id:custId }};
+function jwtGenerator(id) {
+    const payload = { customer: { customerId:id }};
     return jwt.sign(payload, SECRET, {expiresIn: EXPIRATION});
 }
 
-module.exports = jwtGenerator;
+function jwtGeneratorStaff(id, manager) {
+    const payload = { staff: { staffId:id, isManager:manager }};
+    return jwt.sign(payload, SECRET, {expiresIn: EXPIRATION});
+}
+module.exports = jwtGenerator, jwtGeneratorStaff;
