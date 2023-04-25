26cb9a3972ea9a846e2bed2c87842832981f4815
Najmi Shahir <sc21anbm@leeds.ac.uk>
2023-04-25 19:15:28 +0100

    same activity diff day and time: modified

--- server/routes/activity.js
+++ server/routes/activity.js
@@ -5,7 +5,7 @@ const Facility  = require("../database/models/facility");
 const verifyManager = require("../middleware/verifyManager");
 
 // 1. Add new activities (only for manager)
-router.post("/activityid", /*verifyManager,*/ async (req, res, next) => {
+router.post("/activityid", verifyManager, async (req, res, next) => {
     const { name, price, facilityName, day, start, end } = req.body;
     try {
         const facility = await Facility.findByPk(facilityName);
@@ -13,7 +13,7 @@ router.post("/activityid", /*verifyManager,*/ async (req, res, next) => {
             return res.status(404).send("Facility not found");
 
         // check if activity already exist
-        const existingActivity = await Activity.findOne({ where: {activityName: name, facilityName:facilityName}});
+        const existingActivity = await Activity.findOne({ where: {activityName: name, facilityName:facilityName, day: day, startTime: start, endTime: end}});
         if (existingActivity) 
             return res.status(401).send("Activity already exists");
 
@@ -32,7 +32,7 @@ router.post("/activityid", /*verifyManager,*/ async (req, res, next) => {
 });
 
 // 2. Update an existing activity (only for manager)
-router.put("/:id", /*verifyManager,*/ async (req, res, next) => {
+router.put("/:id", verifyManager, async (req, res, next) => {
     try {
         const updateActivity = await Activity.findByPk(req.params.id);
         const updatedActivity = await updateActivity.update(req.body);
