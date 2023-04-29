d2e565b57124864bc8156265892a1f9d0d0501cd
Najmi Shahir <sc21anbm@leeds.ac.uk>
2023-04-29 20:59:50 +0100

    discount route

--- server/routes/discount.js
+++ server/routes/discount.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Discount  = require("../database/models/discount");
 
+// 1. new discount
 router.post('/', async (req, res, next) => {
     try {
         const { discount } = req.body;
@@ -15,5 +16,40 @@ router.post('/', async (req, res, next) => {
         next(err);
     }
 });
+
+// 2. update discount
+router.put('/:id', async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const { discount } = req.body;
+
+        // find the Discount record to update
+        const discountToUpdate = await Discount.findByPk(id);
+        if (!discountToUpdate) {
+            return res.status(404).json('Discount not found');
+        }
+
+        // update the discount value
+        discountToUpdate.discount = discount;
+        await discountToUpdate.save();
+
+        return res.status(200).json(discountToUpdate);
+
+    } catch (err) {
+        next(err);
+    }
+});
+
+// 3. Get discount
+router.get('/', async (req, res) => {
+    try {
+        const discount = await Discount.findOne();
+        res.json(discount);
+    } catch (err) {
+        next(err)
+    }
+});
+
+module.exports = router;
 
 module.exports = router;
