5ff9a56a30e04f7841be22921330156cd4ee5f6f
Edmund Chia <sc21ewkc@leeds.ac.uk>
2023-05-01 23:01:40 +0100

    Fixed css and removed buttons in studio calendar

--- client/src/components/pages/individual-facilities/FacilityPage.js
+++ client/src/components/pages/individual-facilities/FacilityPage.js
@@ -164,7 +164,7 @@ const handleBooking = async () => {
   };
 
   return (
-    <div>
+    <div className="bookingFacilityPage">
       <Navbar />
         <div className="facilityPageContainer">
           <div className="facilityPageWrapper">
