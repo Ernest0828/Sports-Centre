3501f32acac94678f91913d6358b4ebf2449c6a3
Edmund Chia <sc21ewkc@leeds.ac.uk>
2023-05-02 01:43:35 +0100

    fixed css

--- server/database/db.js
+++ server/database/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize("gymCorp", "postgres", "hogwarts", 
+const db = new Sequelize("gymCorp", "postgres", "23072003", 
 {
   host : "localhost",
   dialect : "postgres",
